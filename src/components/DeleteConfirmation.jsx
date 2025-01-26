8dc942415942e993422538f1968c8ff0cd68f3d2
Flame787 <mbrezovic77@gmail.com>
2025-01-26 02:14:22 +0100

    timer edited

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log("TIMER SET");
+    // Adding a timer that Modal with question dissapears after 5 sec,
+    // and selected location automatically get's deleted from saved locations:
+    // setTimeout has 2 arguments: a function, and a duration in miliseconds
 
-// Adding a timer that Modal with question dissapears after 5 sec, 
-// and selected location automatically get's deleted from saved locations:
-// setTimeout has 2 arguments: a function, and a duration in miliseconds
+    // If <DeleteConfirmation> is always part of the DOM (always rendering),
+    // then the timer is always activated whenever the App component renders for the 1st time
+    const timer = setTimeout(() => {
+      onConfirm(); // calling the same function, as if we have confirmed the deleting of the selected location
+    }, 5000);
 
-// If <DeleteConfirmation> is always part of the DOM (always rendering), 
-// then the timer is always activated whenever the App component renders for the 1st time
-setTimeout(() => {
-  onConfirm();   // calling the same function, as if we have confirmed the deleting of the selected location
-}, 5000);
+    return() => {
+      clearTimeout(timer);
+    }
+  }, []);
 
   return (
     <div id="delete-confirmation">
